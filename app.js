92f86d6cf8ccbee7acf872030b50c08b25bcbe17
AlfredoPegueroC <alfredocapellan20@gmail.com>
2024-08-06 22:12:43 -0400

    Done

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const navbar = document.querySelector("#navbar")
 const btn = document.querySelector(".hamburger_menu")
+const menu = document.querySelector(".navbar")
 
 
 btn.addEventListener("click", e => {
   navbar.classList.toggle("active")
+  menu.classList.toggle("active")
 
   if(btn.attributes.src.value == "./images/icon-hamburger.svg"){
     btn.src = "./images/icon-close.svg"
